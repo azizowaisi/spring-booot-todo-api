09e7b62a625ee45fe1e44435852cecd10d44a4d0
“Abdul <azizowaisi@gmail.com>
2024-05-25 16:30:08 +0200

    first branch

---
+++ src/main/java/com/teckiz/todo/todo_web_services/TodoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.teckiz.todo.todo_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teckiz/todo/todo_web_services/TodoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teckiz.todo.todo_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
