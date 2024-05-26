1c538e0ead870acc339b20a327dc929a546d990f
“Abdul <azizowaisi@gmail.com>
2024-05-26 21:10:00 +0200

    spring mvc error structure and handle

---
+++ src/main/java/com/teckiz/todo/todo_web_services/user/UserResource.java
@@ -0,0 +1,45 @@
+package com.teckiz.todo.todo_web_services.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private final UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retriveUser(@PathVariable int id) throws UserPrincipalNotFoundException {
+        User user =  userDaoService.findOne(id);
+
+        if(user == null){
+            throw new UserNotFoundException("id:" + id);
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
